bed977c8efcf5c23e0be3d70fdc67941ba0a0fb6
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-03-08 18:53:44 -0400

    Creacion de entidad ProyectoDetalle

---
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/Entidades/ProyectoDetalle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Parcial2_ap1_2017_0826.Entidades
+{
+    class ProyectoDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TipoTareaId { get; set; }
+        public string Descripcion { get; set; }
+        public int Minutos { get; set; }
+
+        public ProyectoDetalle()
+        {
+            Id = 0;
+            TipoTareaId = 0;
+            Descripcion = string.Empty;
+            Minutos = 0;
+
+        }
+
+        public ProyectoDetalle(int id, int tipoTareaId, string descripcion, int minutos)
+        {
+            Id = id;
+            TipoTareaId = tipoTareaId;
+            Descripcion = descripcion;
+            Minutos = minutos;
+
+        }
+
+    }
+}
