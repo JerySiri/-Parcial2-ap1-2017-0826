827436081e73748ffaa0b7cead2cd2fd66031510
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-03-08 19:22:30 -0400

    Creacion de la carpeta Dal y La entidad Contexto

---
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/DAL/Contexto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_ap1_2017_0826.Entidades;
+
+namespace Parcial2_ap1_2017_0826.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<TipoTarea> TipoTarea { get; set; }
+        public DbSet<ProyectosDetalle> ProyectosDetalle { get; set; }
+        public DbSet<Proyectos> Proyectos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Proyectos.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TipoTarea>().HasData(
+                
+                new TipoTarea() { TareaId = 1, Nombre = "Analisis" },
+                new TipoTarea() { TareaId = 2, Nombre = "Diseño" },
+                new TipoTarea() { TareaId = 3, Nombre = "Programación" },
+                new TipoTarea() { TareaId = 4, Nombre = "Prueba" }
+
+           );
+        }
+    }
+}
