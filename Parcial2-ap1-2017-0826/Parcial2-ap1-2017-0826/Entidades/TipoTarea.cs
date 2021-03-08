5e4dcfb15b8aa37edd7ee95d0afba468bfd6d9ab
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-03-08 18:44:55 -0400

    Creacion de entidad Tipo Tarea

---
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/Entidades/TipoTarea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Parcial2_ap1_2017_0826.Entidades
+{
+    class TipoTarea
+    {
+        [Key]
+        public int TareaId { get; set; }
+        public string Nombre { get; set; }
+
+        public TipoTarea()
+        {
+            TareaId = 0;
+            Nombre = string.Empty;
+        }
+    }
+}
