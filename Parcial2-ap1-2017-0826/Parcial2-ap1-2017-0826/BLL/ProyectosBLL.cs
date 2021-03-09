b0feff163826510b829032a35742536eb4d49d03
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-03-08 20:09:34 -0400

    Creacion De ProyectosBLL

---
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/BLL/ProyectosBLL.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Parcial2_ap1_2017_0826.Entidades;
+using Parcial2_ap1_2017_0826.DAL;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Parcial2_ap1_2017_0826.BLL
+{
+    class ProyectosBLL
+    {
+        public static bool Guardar(Proyectos proyecto)
+        {
+            if (!Existe(proyecto.ProyectoId))
+                return insertar(proyecto);
+            else
+                return modificar(proyecto);
+        }
+
+        private static bool Existe( int id )
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                paso = contexto.Proyectos.Any(p => p.ProyectoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool insertar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            
+            try
+            {
+                contexto.Proyectos.Add(proyecto);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool modificar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete From ProyectosDetalle Where ProyectoId= {proyecto.ProyectoId}");
+
+                foreach (var anterior in proyecto.ProyectoDetalle)
+                {
+                    contexto.Entry(anterior).State = EntityState.Added;
+                }
+                contexto.Entry(proyecto).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id )
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var eliminar = contexto.Proyectos.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Proyectos Buscar(int id)
+        {
+            Proyectos proyectos = new Proyectos();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                proyectos = contexto.Proyectos.Include(p => p.ProyectoDetalle).Where(o => o.ProyectoId == id).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            
+            return proyectos;
+        }
+
+        public static List<Proyectos> GetList(Expression<Func<Proyectos, bool>> proyecto)
+        {
+            List<Proyectos> lista = new List<Proyectos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Proyectos.Where(proyecto).ToList();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+
+        public static List<Proyectos> GetProyectos()
+        {
+            List<Proyectos> proyectos = new List<Proyectos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                proyectos = contexto.Proyectos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return proyectos;
+
+        }
+
+    }
+}
--- Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/Entidades/ProyectosDetalle.cs
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/Entidades/ProyectosDetalle.cs
@@ -9,6 +9,7 @@ namespace Parcial2_ap1_2017_0826.Entidades
     {
         [Key]
         public int Id { get; set; }
+        public int ProyectoId { get; set; }
         public int TipoTareaId { get; set; }
         public string Descripcion { get; set; }
         public int Minutos { get; set; }
@@ -16,14 +17,16 @@ namespace Parcial2_ap1_2017_0826.Entidades
         public ProyectosDetalle()
         {
             Id = 0;
+            ProyectoId = 0;
             TipoTareaId = 0;
             Descripcion = string.Empty;
             Minutos = 0;
         }
 
-        public ProyectosDetalle(int id, int tipoTareaId, string descripcion, int minutos)
+        public ProyectosDetalle(int id,int proyectoId,int tipoTareaId, string descripcion, int minutos)
         {
             Id = id;
+            ProyectoId = proyectoId;
             TipoTareaId = tipoTareaId;
             Descripcion = descripcion;
             Minutos = minutos;
