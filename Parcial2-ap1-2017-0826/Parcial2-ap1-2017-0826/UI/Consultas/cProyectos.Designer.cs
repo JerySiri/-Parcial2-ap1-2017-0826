6ab6ff2a0e68ede86c97c406eeda7ecb2dfd9c1d
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-03-09 13:29:12 -0400

    Modificacion de la consulta cProyecto

--- Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/UI/Consultas/cProyectos.Designer.cs
+++ Parcial2-ap1-2017-0826/Parcial2-ap1-2017-0826/UI/Consultas/cProyectos.Designer.cs
@@ -29,84 +29,95 @@ namespace Parcial2_ap1_2017_0826.UI.Consultas
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cProyectoForm));
+            this.FiltroLabel = new System.Windows.Forms.Label();
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
+            this.CriterioLabel = new System.Windows.Forms.Label();
+            this.CriterioTextBox = new System.Windows.Forms.TextBox();
+            this.BucarButton = new System.Windows.Forms.Button();
+            this.ConsultaDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultaDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // FiltroLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.FiltroLabel.AutoSize = true;
+            this.FiltroLabel.Location = new System.Drawing.Point(12, 13);
+            this.FiltroLabel.Name = "FiltroLabel";
+            this.FiltroLabel.Size = new System.Drawing.Size(40, 15);
+            this.FiltroLabel.TabIndex = 0;
+            this.FiltroLabel.Text = "Filtro: ";
             // 
-            // comboBox1
+            // FiltroComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(13, 31);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(135, 23);
-            this.comboBox1.TabIndex = 1;
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Location = new System.Drawing.Point(13, 31);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(135, 23);
+            this.FiltroComboBox.TabIndex = 1;
             // 
-            // label2
+            // CriterioLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(152, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.CriterioLabel.AutoSize = true;
+            this.CriterioLabel.Location = new System.Drawing.Point(152, 13);
+            this.CriterioLabel.Name = "CriterioLabel";
+            this.CriterioLabel.Size = new System.Drawing.Size(46, 15);
+            this.CriterioLabel.TabIndex = 2;
+            this.CriterioLabel.Text = "Criterio";
             // 
-            // textBox1
+            // CriterioTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(154, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(379, 23);
-            this.textBox1.TabIndex = 3;
+            this.CriterioTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CriterioTextBox.Location = new System.Drawing.Point(155, 31);
+            this.CriterioTextBox.Name = "CriterioTextBox";
+            this.CriterioTextBox.Size = new System.Drawing.Size(276, 23);
+            this.CriterioTextBox.TabIndex = 3;
             // 
-            // button1
+            // BucarButton
             // 
-            this.button1.Location = new System.Drawing.Point(539, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 24);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BucarButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BucarButton.Image = ((System.Drawing.Image)(resources.GetObject("BucarButton.Image")));
+            this.BucarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BucarButton.Location = new System.Drawing.Point(437, 30);
+            this.BucarButton.Name = "BucarButton";
+            this.BucarButton.Size = new System.Drawing.Size(116, 24);
+            this.BucarButton.TabIndex = 4;
+            this.BucarButton.Text = "Buscar         ";
+            this.BucarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BucarButton.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // ConsultaDataGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 61);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(643, 249);
-            this.dataGridView1.TabIndex = 5;
+            this.ConsultaDataGridView.AllowUserToAddRows = false;
+            this.ConsultaDataGridView.AllowUserToDeleteRows = false;
+            this.ConsultaDataGridView.AllowUserToOrderColumns = true;
+            this.ConsultaDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConsultaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultaDataGridView.Location = new System.Drawing.Point(12, 61);
+            this.ConsultaDataGridView.Name = "ConsultaDataGridView";
+            this.ConsultaDataGridView.ReadOnly = true;
+            this.ConsultaDataGridView.RowTemplate.Height = 25;
+            this.ConsultaDataGridView.Size = new System.Drawing.Size(541, 165);
+            this.ConsultaDataGridView.TabIndex = 5;
             // 
             // cProyectoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 320);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(565, 236);
+            this.Controls.Add(this.ConsultaDataGridView);
+            this.Controls.Add(this.BucarButton);
+            this.Controls.Add(this.CriterioTextBox);
+            this.Controls.Add(this.CriterioLabel);
+            this.Controls.Add(this.FiltroComboBox);
+            this.Controls.Add(this.FiltroLabel);
+            this.MinimumSize = new System.Drawing.Size(581, 275);
             this.Name = "cProyectoForm";
             this.Text = "Consulta De Proyectos";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultaDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,11 +125,11 @@ namespace Parcial2_ap1_2017_0826.UI.Consultas
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label FiltroLabel;
+        private System.Windows.Forms.ComboBox FiltroComboBox;
+        private System.Windows.Forms.Label CriterioLabel;
+        private System.Windows.Forms.TextBox CriterioTextBox;
+        private System.Windows.Forms.Button BucarButton;
+        private System.Windows.Forms.DataGridView ConsultaDataGridView;
     }
 }
